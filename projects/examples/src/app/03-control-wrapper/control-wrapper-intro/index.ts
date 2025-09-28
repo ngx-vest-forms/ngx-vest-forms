95e95b412cc3f3ba0286c83d6d9f2e7e6e39f886
The Ult <ult_dev@pm.me>
2025-09-29 00:43:45 +0200

    chore(examples): remove old examples

--- projects/examples/src/app/03-control-wrapper/control-wrapper-intro/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './control-wrapper-intro.page';
-export * from './control-wrapper-intro.manual-form';
-export * from './control-wrapper-intro.wrapper-form';
-export * from './control-wrapper-intro.validations';
-export * from './control-wrapper-intro.content';
